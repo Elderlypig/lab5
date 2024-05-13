c71619b2a8e44e0206223616cb700ad241bdc04c
Elderlypig <egorprokuda742@gmail.com>
2024-05-13 16:34:47 +0300

    first commit

---
+++ 3.js
@@ -0,0 +1,11 @@
+let num, text;
+
+do {
+    num = parseInt(prompt("Введите число больше 100"), 10);
+    text = prompt("Введите текст");
+} while (num <= 100 && num !== null);
+
+if (num > 100) {
+    const words = text.split(' ');
+    alert(words[0]);
+}
---
+++ 4.js
@@ -0,0 +1,9 @@
+let mass = [];
+
+for(let i = 2; i <= 100; i++){
+    if(i % 3 == 0 || i % 8 == 0){
+        mass.push(i);
+    }
+}
+
+alert(mass.join(", "));
